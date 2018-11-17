32daf30ba28b0615e0a66f9434d20a9f4b2c66d5
Jason van Raamsdonk <C17764691@mydit.ie>
2018-11-17 23:52:16 +0000

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,123 @@
+<?php include "db.php"; ?>
+
+<?php
+
+function readRows()
+{
+    global $connection;
+    
+    $query = "SELECT * FROM users";
+    
+    $result = mysqli_query($connection , $query);
+    
+    if(!$result)
+    {
+        die('Query FAILED' . mysqli_error());
+    }
+    
+    while($row = mysqli_fetch_assoc($result))
+    {
+        print_r($row);
+    }
+}
+
+
+function createRows()
+{
+    if(isset($_POST['submit']))
+    {   
+        global $connection;
+        
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        $query = "INSERT INTO users(username, password) ";
+        $query .= "VALUES ('$username', '$password')";
+
+        $result = mysqli_query($connection , $query);
+
+        if(!$result)
+        {
+            die('Query FAILED' . mysqli_error());
+        }
+        else
+        {
+            echo "recorded created";
+        }
+    }
+}
+
+
+function showAllData()
+{
+    global $connection;
+    
+    $query = "SELECT * FROM users";
+    $result = mysqli_query($connection , $query);
+
+    if(!$result)
+    {
+        die('Query FAILED' . mysqli_error());
+    }
+
+
+    while($row = mysqli_fetch_assoc($result))
+    {
+        $id = $row['id'];
+
+        echo "<option value='$id'>$id</option>";
+    }
+}
+
+
+function UpdateTable()
+{
+    if(isset($_POST['submit']))
+    {
+        global $connection;
+
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $id = $_POST['id'];
+
+        $query = "UPDATE users SET ";
+        $query .= "username = '$username', ";
+        $query .= "password = '$password' ";
+        $query .= "WHERE id = $id ";
+
+        $result = mysqli_query($connection, $query);
+
+        if(!$result)
+        {
+            die("QUERY FAILED" . mysqli_error($connection));
+        }
+        else
+        {
+            echo "Record updated";
+        }
+    }
+}
+
+
+function deleteRows()
+{
+    if(isset($_POST['submit']))
+    {
+        global $connection;
+
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $id = $_POST['id'];
+
+        $query = "DELETE FROM users ";
+        $query .= "WHERE id = $id ";
+
+        $result = mysqli_query($connection, $query);
+
+        if(!$result)
+        {
+            die("QUERY FAILED" . mysqli_error($connection));
+        }
+    }
+}
+?>
---
+++ login.php
@@ -0,0 +1,66 @@
+<?php
+
+if(isset($_POST['submit']))
+{
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    
+
+    $connection = mysqli_connect('localhost', 'root', '', 'loginapp');
+    
+    if($connection)
+    {
+        echo "We are connected";
+    }
+    else
+    {
+        echo "Database connection failed";
+    }
+        
+//    if($username && $password)
+//    {
+//        echo $username;
+//        echo $password;
+//    }
+//    else
+//    {
+//        echo "No details entered";
+//    }
+}
+
+?>
+
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+</head>
+<body>
+  
+<div class="container">
+    
+    <div class="col-sm-6">
+        <form action="login.php" method="post">
+            <div class="form-group">
+            <label for="username">Username</label>
+            <input type="text" name="username" class="form-control">
+            </div>
+            
+            <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" name="password" class="form-control">
+            </div>
+            
+            <input class="btn btn-primary" type="submit" name="submit" value="Submit">
+            
+        </form>
+    </div>
+    
+    
+    
+</div>
+       
+</body>
+</html>
